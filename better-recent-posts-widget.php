4624964f5b8eb04455937ca30be60ad3d4ec39da
dgewirtz <dgewirtz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-21 08:02:40 +0000

    Typo fix
    git-svn-id: https://plugins.svn.wordpress.org/better-recent-posts-widget/trunk@1117376 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- better-recent-posts-widget.php
+++ better-recent-posts-widget.php
@@ -3,7 +3,7 @@
 Plugin Name: Better Recent Posts Widget
 Plugin URI: http://zatzlabs.com/better-recent-posts-widget/
 Description: Provides a better recent posts widget, including thumbnails, category, and number options. Plugin adopted from Pippin Williamson.
-Version: 1.1.7
+Version: 1.1.8
 Author: David Gewirtz
 Author URI: http://zatzlabs.com/lab-notes/
 */
